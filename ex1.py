dfb6ae24abd150c4dc918129a051fd97e23ec73c
Likhesh Mahajan <likhesh.mahajan@gmail.com>
2017-09-24 01:31:02 +1000

    first commit

---
+++ ex1.py
@@ -0,0 +1,7 @@
+index=5
+while index < 0:
+	index +=1
+	if index %2 ==1:
+		continue
+        print index
+print "Fuck off"
---
+++ ex2.py
@@ -0,0 +1,7 @@
+x= input ("Enter a value: ")
+if x == 1:
+	print "You entered one"
+elif x ==2:
+	print "You entered two"
+else:
+	print "You entered whatever "
